036419aa862c414df4e8f0d2464642580ca650c0
din1030 <din1030@gmail.com>
2015-12-18 16:51:16 +0800

    Initial Commit

---
+++ CintinWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CintinWork
+//
+//  Created by din1030 on 2015/12/18.
+//  Copyright © 2015年 Cheng Chia Ting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CintinWork/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CintinWork
+//
+//  Created by din1030 on 2015/12/18.
+//  Copyright © 2015年 Cheng Chia Ting. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
